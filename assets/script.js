698f96cd20a1aa2375db979d140367a282d4f6db
Ahmed Magbool <ahmad209a@gmail.com>
2023-10-09 22:47:15 +0400

    Magbool Portfolio V1

---
+++ assets/script.js
@@ -0,0 +1,60 @@
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navItem = document.querySelectorAll(".nav__item"),
+  header = document.getElementById("header");
+
+// Open and close menu
+
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav__menu--open");
+  changeIcon();
+});
+
+// close the window
+navItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (navMenu.classList.contains("nav__menu--open")) {
+      navMenu.classList.remove("nav__menu--open");
+    }
+    changeIcon();
+  });
+});
+
+// Change nav toggle icon
+function changeIcon() {
+  if (navMenu.classList.contains("nav__menu--open")) {
+    navToggle.classList.replace("ri-menu-3-fill", "ri-close-line");
+  } else {
+    navToggle.classList.replace("ri-close-line", "ri-menu-3-fill");
+  }
+}
+
+//Testimonials Slider
+
+const testimonialsSlider = new Swiper(".testimonials__wrapper", {
+  loop: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+  effect: "coverflow",
+  grabCursor: true,
+  slidesPerView: 1,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    520: {
+      slidesPerView: "auto",
+    },
+  },
+});
+
+// Header Scroll Animation
